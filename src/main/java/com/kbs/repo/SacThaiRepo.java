d8b5c0456db983e5f5cb5f0847702015ee0fd44c
Thịnh Trần Nguyên <84261217+thinh1407@users.noreply.github.com>
2022-01-12 18:56:15 +0700

    Update SacThaiRepo.java

--- src/main/java/com/kbs/repo/SacThaiRepo.java
+++ src/main/java/com/kbs/repo/SacThaiRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 public interface SacThaiRepo extends JpaRepository<SacThai, Integer> {
-    @Query(value = "CALL get_salary_nurse(:icam_id,:ilong_may_id,:ima_id,:imat_id,:imoi_id,:imui_id,:irang_id,:itran_id);", nativeQuery = true)
+    @Query(value = "CALL check_sac_thai(:icam_id,:ilong_may_id,:ima_id,:imat_id,:imoi_id,:imui_id,:irang_id,:itran_id);", nativeQuery = true)
     List<Float> sacThaiSimilar(@RequestParam("icam_id") int icam_id, @RequestParam("ilong_may_id") int ilong_may_id,
                                @RequestParam("ima_id") int ima_id, @RequestParam("imat_id") int imat_id,
                                @RequestParam("imoi_id") int imoi_id, @RequestParam("imui_id") int imui_id,
