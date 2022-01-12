94e752b08b4daa766dce7b254cfd7acf82c986da
namchtq238 <connorkenway238@gmail.com>
2022-01-12 15:23:16 +0700

    add model

--- src/main/java/com/kbs/MainController.java
+++ src/main/java/com/kbs/MainController.java
@@ -1,6 +1,8 @@
 package com.kbs;
 
+import com.kbs.model.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,7 +12,15 @@ public class MainController {
         return "index";
     }
     @GetMapping("/input")
-    public String getInputPage(){
+    public String getInputPage(Model model){
+        model.addAttribute("cam", new Cam());
+        model.addAttribute("longmay", new LongMay());
+        model.addAttribute("ma", new Ma());
+        model.addAttribute("mat", new Mat());
+        model.addAttribute("moi", new Moi());
+        model.addAttribute("mui", new Mui());
+        model.addAttribute("rang", new Rang());
+        model.addAttribute("tran", new Tran());
         return "input";
     }
 }
